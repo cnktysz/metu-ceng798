d599548f5084a0110b9f1c1cb79a8da58d8a0472
Cenk Tüysüz <cenktuysuz@gmail.com>
2020-04-08 00:25:34 +0300

    added review and single_qubit_ops notebooks

--- .animations/bloch.py
+++ .animations/bloch.py
@@ -1,4 +1,6 @@
 from qiskit.visualization import plot_bloch_vector
+from qiskit.visualization import plot_bloch_multivector
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
@@ -10,12 +12,30 @@ def delete_all_logs(log_dir):
 		if item.endswith('.png'):
 			os.remove(log_dir+item)
 
+def plot_2_qubits():
+	fig = plot_bloch_multivector([0,1,0,0])
+	fig.savefig('png/out.png')
+	plt.close()	
 
-def rotate_y(i):
-	fig = plot_bloch_vector([np.sin((i/180)*np.pi),0,np.cos((i/180)*np.pi)], title="New Bloch Sphere")
-	fig.savefig('png/out_%4.d.png'%i)
+
+def vector():
+	fig = plot_bloch_vector([0,-1,0])
+	fig.plot([1,0],[0,0])
+	fig.savefig('png/out.png')
+	plt.close()	
+
+def rotate_y(i,v):
+	fig = plot_bloch_vector([np.sin((i/180)*np.pi)*np.cos((v/180)*np.pi),np.sin((i/180)*np.pi)*np.sin((v/180)*np.pi),np.cos((i/180)*np.pi)])
+	fig.savefig('png/to_gif/out_%4.d.png'%i)
 	plt.close()
 
-delete_all_logs('png/')
-for i in tqdm(range(0,360,10)):
-	rotate_y(i)
+'''
+delete_all_logs('png/to_gif/')
+for i in tqdm(range(0,91,1)):
+	if i < 45:
+		v=i
+	else: 
+		v=90-i
+	rotate_y(i,v)
+'''
+vector()
